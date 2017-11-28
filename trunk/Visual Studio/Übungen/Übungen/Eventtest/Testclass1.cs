4781b490037aab21ffb1c3698aafcce28477113a
mitti2000 <mitti2000@gmail.com>
2017-11-28 20:14:44 +0100

    28.11.17_1

---
+++ trunk/Visual Studio/Übungen/Übungen/Eventtest/Testclass1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventtest
+{
+    //Throws Event in mehtod button press simulator
+    class Testclass1
+
+    {
+        public delegate void Testdelegate(string text);
+        public event Testdelegate MyEvent;
+
+        public void WriteOutput(TextWriter tw)
+        {
+            tw.WriteLine("Hello World");
+        }
+
+        public void ButtonPressSimulator()
+        {
+            Console.WriteLine("Button Pressed");
+            MyEvent?.Invoke("Event sent"); // Wie ist der Syntax zu lesen?
+        }
+    }
+}
---
+++ trunk/Visual Studio/Übungen/Übungen/RomanNumbers/RomanNumerals.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumbers
+{
+    public class RomanNumerals
+    {
+        public string ArabicToRoman(int arabic)
+        {
+            string result = "";
+            string[] romanNumbers = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+            int[] faktoren = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+            int zahl = arabic;
+
+            for (int i = 0; i < romanNumbers.Length; i++)
+            {
+                while (zahl >= faktoren[i])
+                {
+                    zahl -= faktoren[i];
+                    result += romanNumbers[i];
+                }
+            }
+            return result;
+        }
+    }
+}
