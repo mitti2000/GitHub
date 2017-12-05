16a7c0ec2145ff2184271bf0a8d4b4f259d836f0
mitti2000 <mitti2000@gmail.com>
2017-12-05 20:11:39 +0100

    5.12.17_1

---
+++ trunk/Visual Studio/Übungen/Übungen/MockingExample/FileManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockingExample
+{
+    class FileManager
+    {
+        private IFileWrapper _fileWrapper;
+
+        public FileManager(IFileWrapper fileWrapper)
+        {
+            _fileWrapper = fileWrapper;
+        }
+
+        public void deleteAll()
+        {
+            _fileWrapper.deleteAll();elete(file);
+        }
+
+    }
+
+
+}
