599e7c8ec54b225ba567ff9ce58f4cc81b7edb40
mitti2000 <mitti2000@gmail.com>
2018-04-03 06:48:25 +0200

    3.4.18_1

---
+++ trunk/devstuff/paralleldynamisch/SequentialServer.java
@@ -0,0 +1,56 @@
+import java.net.ServerSocket;
+public class SequentialServer {
+	
+	public static void main(String[] args)
+	{
+		int counter = 0;
+		
+		// create socket
+		try(ServerSocket serversocket = new ServerSocket(2500))
+		{
+			while (true)
+			{
+				// warte auf Verbindungsaufnahme
+				System.out.println("Warten auf Verbindungsaufbau");
+				try(TCPSocket tcpSocket = new TCPSocket(serversocket.accept()))
+				{
+					// Clients ausführen
+					while (true)
+					{
+						String request = tcpSocket.receiveLine();
+						if (request != null)
+						{
+							// Sleep
+							try
+							{
+								int secs = Integer.parseInt(request);
+								Thread.sleep(secs * 1000);
+							}
+							catch (Exception e)
+							{
+								System.out.println(e);
+							}
+							tcpSocket.sendLine(request);
+						}
+						else
+						{
+							System.out.println("Schliessen der Verbindung");
+							break;
+						}
+					}
+				}
+				catch(Exception e)
+				{
+					System.out.println("Verbindung geschlossen");
+					System.out.println(e);
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			System.out.println("Fehler beim Erzeugen oder Nutzen des ServerSockets");
+			return;
+		}
+	}
+
+}
