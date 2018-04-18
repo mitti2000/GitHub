5ff75636066b7dd4dbc718553e88d5e27b17e7df
mitti2000 <mitti2000@gmail.com>
2018-04-18 18:47:27 +0200

    18.4.18_1

--- trunk/Visual Studio/PEA/PEA/Program.cs
+++ trunk/Visual Studio/PEA/PEA/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,10 +19,25 @@ namespace PEA
             if (args.Length > 0)
             {
                 Primesieb _primesieb = new Primesieb(int.Parse(args[0]));
-                foreach (var primeType in _primesieb.Eratosthenes())
+                int counter = 0;
+                int lineCounter = 0;
+                //Primesieb.PrimeType[] primeArray = _primesieb.Eratosthenes();  // All numbers as notPrime or Prime
+                //int[] primeArray = _primesieb.onlyPrimes(_primesieb.Eratosthenes());
+                //List<int> primeArray = _primesieb.onlyPrimesList(_primesieb.Eratosthenes());
+                Dictionary<int, int> primeArray = _primesieb.onlyPrimesDict(_primesieb.Eratosthenes());
+                foreach (var number in primeArray)
                 {
-                    Console.WriteLine(primeType);
+                    counter++;
+                    lineCounter++;
+                    Console.Write(counter + ": " + number + " // ");
+                    if (lineCounter >= 9)
+                    {
+                        lineCounter = 0;
+                        Console.WriteLine();
+                    }
+                    
                 }
+
             }
         }
 
