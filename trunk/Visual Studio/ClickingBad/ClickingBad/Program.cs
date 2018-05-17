3a656c748b1fba64569b68bd3fa444a696992e91
mitti2000 <mitti2000@gmail.com>
2018-05-17 20:12:17 +0200

    17.5.18_1

---
+++ trunk/Visual Studio/ClickingBad/ClickingBad/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClickMyDrugs
+{
+    class Program
+    {
+        private const int MOUSEEVENTF_MOVE = 0x0001; /* mouse move */
+        private const int MOUSEEVENTF_LEFTDOWN = 0x0002; /* left button down */
+        private const int MOUSEEVENTF_LEFTUP = 0x0004; /* left button up */
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x0008; /* right button down */
+
+        static void Main(string[] args)
+        {
+            Thread.Sleep(5000);
+            while (true)
+            {
+                SetCursorPos(100, 220);
+                for (int i = 0; i < 1000; i++)
+                {
+                    Thread.Sleep(10);
+                    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                }
+
+                SetCursorPos(100, 380);
+                for (int i = 0; i < 1000; i++)
+                {
+                    Thread.Sleep(5);
+                    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                }
+            }
+        }
+
+        [DllImport("user32")]
+        public static extern int SetCursorPos(int x, int y);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+    }
+}
