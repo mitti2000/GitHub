982a07f3fc83ce62a1bd42b6a4f8d42ef2ef1485
mitti2000 <mitti2000@gmail.com>
2018-05-31 19:52:10 +0200

    31.5.18_1

--- trunk/Visual Studio/ClickingBad/ClickingBad/Program.cs
+++ trunk/Visual Studio/ClickingBad/ClickingBad/Program.cs
@@ -5,6 +5,8 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Drawing;
 
 namespace ClickMyDrugs
 {
@@ -14,26 +16,61 @@ namespace ClickMyDrugs
         private const int MOUSEEVENTF_LEFTDOWN = 0x0002; /* left button down */
         private const int MOUSEEVENTF_LEFTUP = 0x0004; /* left button up */
         private const int MOUSEEVENTF_RIGHTDOWN = 0x0008; /* right button down */
+        private const int VK_ESCAPE = 0x1B; /* Escape Down */
 
         static void Main(string[] args)
         {
             Thread.Sleep(5000);
+            bool inside = false;
+            int sleepTime = 5;
+
             while (true)
             {
-                SetCursorPos(100, 220);
-                for (int i = 0; i < 1000; i++)
+                while (!inside)
                 {
-                    Thread.Sleep(10);
-                    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
-                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    if (GetCursorPosition().X > 150
+                        && GetCursorPosition().X < 190
+                        && GetCursorPosition().Y > 240
+                        && GetCursorPosition().Y < 430)
+                    {
+                        inside = true;
+                    }
                 }
 
-                SetCursorPos(100, 380);
-                for (int i = 0; i < 1000; i++)
+                while (inside)
                 {
-                    Thread.Sleep(10);
-                    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
-                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+
+                    SetCursorPos(173, 254);
+                    for (int i = 0; i < 500; i++)
+                    {
+                        Thread.Sleep(sleepTime);
+                        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                        if (!(GetCursorPosition().X > 150
+                              && GetCursorPosition().X < 190
+                              && GetCursorPosition().Y > 240
+                              && GetCursorPosition().Y < 430))
+                        {
+                            inside = false;
+                            break;
+                        }
+                    }
+
+                    SetCursorPos(173, 418);
+                    for (int i = 0; i < 1000; i++)
+                    {
+                        Thread.Sleep(sleepTime);
+                        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                        if (!(GetCursorPosition().X > 150
+                              && GetCursorPosition().X < 190
+                              && GetCursorPosition().Y > 240
+                              && GetCursorPosition().Y < 430))
+                        {
+                            inside = false;
+                            break;
+                        }
+                    }
                 }
             }
         }
@@ -41,7 +78,31 @@ namespace ClickMyDrugs
         [DllImport("user32")]
         public static extern int SetCursorPos(int x, int y);
 
+        [DllImport("user32.dll")]
+        public static extern int GetCursorPos(out POINT lpPoint);
+
         [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
         public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        public static Point GetCursorPosition()
+        {
+            POINT lpPoint;
+            GetCursorPos(out lpPoint);
+            //bool success = User32.GetCursorPos(out lpPoint);
+            // if (!success)
+
+            return lpPoint;
+        }
+
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public static implicit operator Point(POINT point)
+            {
+                return new Point(point.X, point.Y);
+            }
+        }
     }
 }
